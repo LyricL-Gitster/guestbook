6ed981d75970096b0dc0b9400f5e17f5c244b484
Lyric <llhupp09@smumn.edu>
2013-02-15 17:46:44 -0600

    -init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '5c6677d106c91bb1adce43246d9ccd1bf30d6b5e090c2270fccf4ed8fc8c2d0ebc82fdb3b079e947a7efec69601cdc557a1fe10fc1b15fecef4b9846977001a2'
