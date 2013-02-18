0d10d7bec526cd7d6710c64d0098d811da53e912
Lyric <llhupp09@smumn.edu>
2013-02-17 20:03:58 -0600

    -reloaded old seeds file -changed type for message from string to text

--- db/migrate/20130216191051_create_posts.rb
+++ db/migrate/20130216191051_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :message
+      t.text :message
       t.string :location
       t.integer :user_id
 
